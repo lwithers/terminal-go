8804e976c7b841bc3bdc79ced0642933e9fcfc88
Laurence Withers <l@lwithers.me.uk>
2016-06-06 21:50:35 +0100

    screen: add DrawString

--- screen/draw.go
+++ screen/draw.go
@@ -22,6 +22,41 @@ func DrawRune(r rune, fg, bg uint32, x, y int) {
 	buf.bg[pos] = bg
 }
 
+// DrawString modifies the screen buffer by writing the given UTF-8 string with
+// fg/bg text/background colours at the given x, y coordinates. The string is
+// length limited to the given max runes; pass 0 or negative to limit at the
+// edge of the screen.
+func DrawString(s string, fg, bg uint32, x, y, max int) {
+	pos := y*buf.w + x
+	if max <= 0 || max > buf.w-x {
+		max = buf.w - x
+	}
+
+	var count int
+	for _, r := range s {
+		buf.runes[pos+count] = r
+		buf.fg[pos+count] = fg
+		buf.bg[pos+count] = bg
+		count++
+		if count >= max {
+			break
+		}
+	}
+
+	if x < buf.dmgX1 {
+		buf.dmgX1 = x
+	}
+	if x+count >= buf.dmgX2 {
+		buf.dmgX2 = x + count + 1
+	}
+	if y < buf.dmgY1 {
+		buf.dmgY1 = y
+	}
+	if y >= buf.dmgY2 {
+		buf.dmgY2 = y + 1
+	}
+}
+
 // Flush writes the contents of the buffer to the screen.
 func Flush() {
 	buf.drawBuffer()
